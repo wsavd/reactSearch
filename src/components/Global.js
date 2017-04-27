d82b99a5d4e804ab5410af432835053f89467451
wsavd <tuve@rootfest.net>
2017-04-27 09:04:17 +0300

    components/global

---
+++ src/components/Global.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Global extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Hello from Global component</h2>
+      </div>
+    );
+  }
+}
+
+export default Global;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Global from './components/Global';
+import './index.css';
+
+ReactDOM.render(
+  <Global />,
+  document.getElementById('root')
+);
