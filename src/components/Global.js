e0ed2585690457b00d17547f295348406c8744bc
wsavd <tuve@rootfest.net>
2017-04-27 11:36:32 +0300

    search()

--- src/components/Global.js
+++ src/components/Global.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class Global extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+
+  search(){
+    const url = 'https://www.googleapis.com/books/v1/volumes?q=HarryPotter';
+
+    fetch(`${url}`,{method: 'GET'})//буква йо x_X
+      .then(res => res.json())
+      .then(json => console.log(json/*.body*/))
+  }
+
   render() {
     return (
-      <div>
-        <h2>Hello from Global component</h2>
+      <div className="App">
+        <h2 className="title">Book Explorer</h2>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Search for a Book" //по умолчанию поле равно значению placeholder состояние пустое
+              onChange={event => this.setState({query: event.target.value})}//когда значение placeholder изменено устанавливаем новое состояние для query
+              onKeyPress={event => {
+                if (event.key == "Enter") {
+                  this.search()
+                }
+              }}
+            />
+          </InputGroup>
+        </FormGroup>
       </div>
-    );
+    )
   }
 }
 
